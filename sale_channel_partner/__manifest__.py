6ed9cabe5ad28937547168fdaf8f6435723c836b
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-16 11:57:30 -0300

    [MIG] sale_channel_partner: Migration to 17.0

---
+++ sale_channel_partner/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Sale Channel Partner",
+    "summary": "Bind sale channels to contacts",
+    "version": "17.0.0.0.0",
+    "category": "Generic Modules/Sale",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-channel",
+    "depends": ["sale_channel"],
+    "license": "AGPL-3",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/sale_channel_view.xml",
+        "views/res_partner_view.xml",
+        "views/sale_channel_partner_view.xml",
+    ],
+    "demo": ["demo/demo.xml"],
+    "installable": True
+}
