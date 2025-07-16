47dbb04f41d0bd3480e84f0075f2890f3c50b00b
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-16 11:57:30 -0300

    [MIG] sale_channel_product: Migration to 17.0

---
+++ sale_channel_product/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Sale Channel Product",
+    "summary": "Link Product with sale channel",
+    "version": "17.0.1.0.1",
+    "category": "Sale Channel",
+    "website": "https://github.com/OCA/sale-channel",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": []
+    },
+    "depends": ["sale_channel"],
+    "data": ["views/product_template_view.xml"],
+    "demo": []
+}
