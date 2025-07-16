089a9bda007b3538d03dc233562a96d68ce017aa
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2025-07-16 11:57:30 -0300

    [MIG] sale_channel_search_engine: Migration to 17.0

---
+++ sale_channel_search_engine/README.rst
@@ -0,0 +1,9 @@
+.. image:: https://odoo-community.org/readme-banner-image
+   :target: https://odoo-community.org/get-involved?utm_source=readme
+   :alt: Odoo Community Association
+
+==========================
+Sale Channel Search Engine
+==========================
+
+...existing code...
---
+++ sale_channel_search_engine/models/se_backend.py
@@ -0,0 +1,13 @@
+# Copyright 2023 Akretion (https://www.akretion.com).
+# @author Raphaël Reverdy<raphael.reverdy@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+class SeBackend(models.Model):
+    _inherit = "se.backend"
+
+    sale_channel_id = fields.One2many(
+        comodel_name="sale.channel",
+        inverse_name="search_engine_id",
+    )
